1666af977a042a3a12b81b19fa6cf2537fa1434c
Patrick Mylund Nielsen <patrick@patrickmylund.com>
2011-02-14 20:25:08 +0100

    Use Exception if file is not found; more flexible separator; more logical naming

--- emailsfromfile.py
+++ emailsfromfile.py
@@ -8,32 +8,33 @@
   License: WTFPL (http://sam.zoy.org/wtfpl/)
 '''
 
+__version__ = '1.1'
+
 import sys
 import os
 import re
 import codecs
 
-__version__ = '1.1'
-
 # Regular expression matching according to RFC 2822 (http://tools.ietf.org/html/rfc2822)
-rfc2822_regex = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
-email_expr = re.compile(rfc2822_regex, re.IGNORECASE)
+rfc2822_re = r"""(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"""
+email_prog = re.compile(rfc2822_re, re.IGNORECASE)
 
 def isEmail(string):
-    return email_expr.match(string)
+    return email_prog.match(string)
 
 def main(filename, separator='\n', encoding=None):
-    results = set()
+    separator_replace = {
+        'space': ' ',
+        'newline': '\n',
+    }
     if not os.path.isfile(filename):
-        print("%s is not a file." % filename)
-        return
-    f = codecs.open(filename, 'rb', encoding)
-    for line in f:
-        results.update(email_expr.findall(line))
-    if separator == 'space':
-        separator = ' '
-    elif separator == 'newline':
-        separator = '\n'
+        raise IOError("%s is not a file." % filename)
+    results = set()
+    with codecs.open(filename, 'rb', encoding) as f:
+        for line in f:
+            results.update(email_prog.findall(line))
+    for k, v in separator_replace.iteritems():
+        separator = separator.replace(k, v)
     print(separator.join(results))
 
 if __name__ == '__main__':
